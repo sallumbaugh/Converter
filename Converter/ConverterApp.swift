54a015a5032149797fba62a3621708424548fae4
Shane Allumbaugh <shane.allumbaugh@outlook.com>
2022-04-20 17:53:38 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Shane Allumbaugh on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
